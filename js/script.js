5a5e4438d2f46e6e67ed1ee2172298e18b7042cf
jruivo <jose.ruivo@elecnor.es>
2020-06-30 10:44:15 +0100

    Items are added to the list

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
-const shopping = document.querySelector('.shopping');
-const list = document.querySelectorAll('.shopping-list');
+const shoppingForm = document.querySelector('.shopping');
+const list = document.querySelector('.shopping-list');
+
+let items = [];
+
+
+function addItem(e) {
+  const item = e.target.item.value;
+  if (!item) return;
+  e.preventDefault();
+
+  items.push({ item, id: Date.now(), checked: false });
+  e.target.item.value = "";
+
+  list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
+function updateItems() {
+  const html = items.map((item) => {
+    return `<li>
+				<input type="checkbox"/>
+				<span>${item.item}</span>
+				<button>X</button>
+		</li>`
+  }).join("");
+  console.log(html);
+  list.innerHTML = html;
+}
+
+
+shoppingForm.addEventListener('submit', addItem)
+
+list.addEventListener('itemsUpdated', updateItems);
